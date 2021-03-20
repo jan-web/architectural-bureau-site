aafc9aff21a879986cb35a8af226904b64b35142
Your Name <you@example.com>
2021-03-20 19:49:07 +0100

    I have tuned some files a litle bit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Landing page of Architectural bureau
 ## I did this work for a web developer competition. The layout of the web design was obtained from the organizer of the competition.
+- You could see this landing page here [https://jan-web.github.io/architectural-bureau-site/index.html](https://jan-web.github.io/architectural-bureau-site/index.html)
--- app/js/main.js
+++ app/js/main.js
@@ -14,36 +14,17 @@ window.addEventListener('DOMContentLoaded', () => {
           menu.classList.toggle('menu_active');
       })
   })
-  // const worksGallery = document.querySelector('.works__gallery');
-
-  // worksGallery.addEventListener('mouseover', (e)=> {
-  //   e.stopImmediatePropagation();
-  //   console.log(e);
-  //    const card = e.target.closest('.works__card');
-  //   card.firstElementChild.classList.add('works__card-active')
-  //   })
 
   const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    // direction: 'vertical',
+
     direction: 'horizontal',
     loop: true,
     slidesPerWiew: 1,
 
-    // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
-
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
 
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
   });
 })
